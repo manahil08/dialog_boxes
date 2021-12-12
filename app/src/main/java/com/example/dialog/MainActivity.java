a83508f1b5f0d8796df535158ed41f740b7bafcf
manahil <bitf18a507@pucit.edu.pk>
2021-12-12 21:51:54 +0500

    first

---
+++ app/src/main/java/com/example/dialog/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.dialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View myLayout=layoutInflater.inflate(R.layout.my_layout,(ViewGroup)findViewById(R.id.dialog_container) );
+
+
+        TextView myMessage=(TextView)myLayout.findViewById(R.id.textViewCameraInfo);
+        myMessage.setText("My Custom Toast");
+        Toast myToast=new Toast(getApplicationContext());
+        myToast.setDuration(Toast.LENGTH_LONG);
+        myToast.setView(myLayout);
+        myToast.show();
+
+
+    }
+}
